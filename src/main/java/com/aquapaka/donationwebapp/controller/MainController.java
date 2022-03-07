aed4e4a95186fb0c61f9961fa19283a7d6a37a62
Tam Long <aqua.tamlong@gmail.com>
2022-03-07 18:10:19 +0700

    remove unused import

--- src/main/java/com/aquapaka/donationwebapp/controller/MainController.java
+++ src/main/java/com/aquapaka/donationwebapp/controller/MainController.java
@@ -18,7 +18,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
